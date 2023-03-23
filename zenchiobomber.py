07f8609c93ccd8fea2c1e51915259989b530115e
Zenchio <80125191+zenchio@users.noreply.github.com>
2023-03-24 00:12:04 +0300

    Update zenchiobomber.py

--- zenchiobomber.py
+++ zenchiobomber.py
@@ -179,9 +179,5 @@ while 1:
         input()
     elif menu == 3:
         system("cls||clear")
-        print(Fore.LIGHTWHITE_EX + " zenchio\n")
-        sleep(12)
-    elif menu == 4:
-        system("cls||clear")
         print(Fore.LIGHTRED_EX + "Çıkış yapılıyor...")
         break
