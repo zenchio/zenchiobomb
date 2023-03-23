3734e080bca06c3dc9f5ce0831c66bcb5411ecc9
Zenchio <80125191+zenchio@users.noreply.github.com>
2023-03-24 00:16:48 +0300

    Update zenchiobomber.py

--- zenchiobomber.py
+++ zenchiobomber.py
@@ -33,7 +33,7 @@ while 1:
     Ara: {}              {}discord -zenon-#3798 by {}@zenchio\n  
     """.format(Fore.LIGHTCYAN_EX, len(servisler_sms), len(servisler_call), Style.RESET_ALL, Fore.LIGHTRED_EX))
     try:
-        menu = (input(Fore.LIGHTMAGENTA_EX + " 1- SMS Gönder\n 2- Ara\n 4- Çıkış\n\n" + Fore.LIGHTYELLOW_EX + " Seçim: "))
+        menu = (input(Fore.LIGHTMAGENTA_EX + " 1- SMS Gönder\n 2- Ara(henüz çalışmıyor)\n 4- Çıkış\n\n" + Fore.LIGHTYELLOW_EX + " Seçim: "))
         if menu == "":
             continue
         menu = int(menu) 
